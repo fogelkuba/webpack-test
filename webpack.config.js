025b1857aafc49416e032b0a7bbe587349425096
fogelkuba <fogelkuba@gmail.com>
2018-11-10 09:37:44 +0100

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,20 @@ module.exports = {
     entry: './src/js/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "bundle.js"
+        filename: "bundle.js",
+        publicPath: "/dist"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                // loader: 'css-loader',
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+
+        ]
     }
 };
